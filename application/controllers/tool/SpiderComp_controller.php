0af7f97b16b1a19b60664ae5146f87719291a159
footerbird <footerbird@163.com>
2019-05-06 14:03:48 +0800

    add 企业信息json

--- application/controllers/tool/SpiderComp_controller.php
+++ application/controllers/tool/SpiderComp_controller.php
@@ -8,11 +8,11 @@ class SpiderComp_controller extends CI_Controller {
   public function index(){//抓取企业资产详情页面
       //可以先手动获取要采集的页面源码
       for($i=481;$i<=500;$i++){
-          $html = file_get_contents('https://www.qichacha.com/g_HAIN_'.$i.'.html');
+          $html = file_get_contents('https://www.qichacha.com/gongsi_area.shtml?prov=ZJ&p='.$i);
           //然后可以把页面源码或者HTML片段传给QueryList
           
           $data = QueryList::Query($html,array(
-            'Link' => array('#searchlist > a','href'),
+            'Link' => array('#searchlist > table > tbody > tr > td:nth-child(2) > a','href'),
           ))->data;
           
           foreach($data as $item){
