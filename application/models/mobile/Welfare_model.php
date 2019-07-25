a279d4d6162952270e48ffce718dec3e1097da31
footerbird <footerbird@163.com>
2019-07-25 17:56:29 +0800

    修改福利状态为active,inactive

--- application/models/mobile/Welfare_model.php
+++ application/models/mobile/Welfare_model.php
@@ -7,7 +7,7 @@ class Welfare_model extends CI_Model {
     }
     
     public function get_welfareList(){//福利列表
-        $sql = "select * from welfare_info where welfare_status = 1 order by publish_time desc";
+        $sql = "select * from welfare_info where welfare_status = 'active' order by publish_time desc";
         $query = $this->db->query($sql);
         return $query->result();
     }
